2bfc09cfadaf2fc0c7373dad252965ed6d2eb804
Nelson <casadocomp97@hotmal.com>
2023-06-08 14:44:31 -0400

    chore:change timezone

--- config/app.php
+++ config/app.php
@@ -68,8 +68,8 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
-
-    'timezone' => 'America/Sao_Paulo',
+  
+    'timezone' => 'America/Porto_Velho',
 
     /*
     |--------------------------------------------------------------------------
